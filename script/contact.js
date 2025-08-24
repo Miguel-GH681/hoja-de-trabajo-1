250143c030be526452df051e2ddaf0cb2f15b90c
alvaro <miguelgh681@gmail.com>
2025-08-23 23:14:33 -0600

    Finalizacion del modulo de contacto

--- script/contact.js
+++ script/contact.js
@@ -3,6 +3,23 @@ const dateInput = document.getElementById('dateInput');
 const emailInput = document.getElementById('emailInput');
 const messageInput = document.getElementById('messageInput');
 const formButton = document.getElementById('formButton');
+const modalBody = document.getElementById('modalBody');
+
+const inputs = document.querySelectorAll('.input-container input, textarea');
+
+inputs.forEach((input)=>{
+    input.addEventListener("blur", ()=>{
+        const name = nameInput.value;
+        const date = dateInput.value;
+        const email = emailInput.value;
+        const message = messageInput.value;
+        emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+        if(name.length > 0 && date.length > 0 && message.length > 0 && emailRegex.test(email)){
+            formButton.setAttribute('data-bs-toggle', 'modal');
+        }
+    });
+});
 
 formButton.addEventListener('click', ()=>{
     const name = nameInput.value;
@@ -11,14 +28,25 @@ formButton.addEventListener('click', ()=>{
     const message = messageInput.value;
     emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-
     if(name.length > 0 && date.length > 0 && message.length > 0 && emailRegex.test(email)){
-        formButton.setAttribute('data-bs-toggle', 'modal');
         console.log(`Nombre: ${name}
         fecha de nacimiento: ${date}
         correo electrónico: ${email}
-        mensaje: ${message}`)
-    } else{
-        console.error('Ingrese correctamente todos los datos'); 
+        mensaje: ${message}`);
+
+        const modalBody = document.getElementById('modalBody');
+        const modalName = document.createElement('p');
+        modalName.textContent = 'Nombre: ' + name;
+        const modalDate = document.createElement('p');
+        modalDate.textContent = 'Fecha de nacimiento: ' + date;
+        const modalEmail = document.createElement('p');
+        modalEmail.textContent = 'Correo electrónico: ' + email;
+        const modalMessage = document.createElement('p');
+        modalMessage.textContent = 'Mensaje: ' + message;
+        
+        modalBody.appendChild(modalName);
+        modalBody.appendChild(modalDate);
+        modalBody.appendChild(modalEmail);
+        modalBody.appendChild(modalMessage);
     }
 });
